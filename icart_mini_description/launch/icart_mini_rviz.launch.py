507c3dcc55192fc6215e45de0681db5c7de6484d
ShunjiHashimoto <h.shunji1206@gmail.com>
2025-02-16 11:41:07 +0900

    Add URDF

---
+++ icart_mini_description/launch/icart_mini_rviz.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('icart_mini_description')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'icart_mini.xacro')
+    rviz_config_file = os.path.join(pkg_share, 'rviz', 'icart_mini.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': Command(['xacro ', xacro_file])}],
+            output='screen'
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     arguments=['-d', rviz_config_file],
+        #     output='screen'
+        # )
+    ])
+
