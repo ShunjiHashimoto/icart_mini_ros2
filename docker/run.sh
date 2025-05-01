078c898df30960c15f435174646581254ad50e6f
ShunjiHashimoto <h.shunji1206@gmail.com>
2025-05-01 17:32:54 +0900

    update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,13 @@
 DOCKER_IMAGE="icart_mini_ros2:latest"
 CONTAINER_NAME="icart_mini_ros2"
 
-docker run --rm -it \
+# 既存コンテナが残ってたら削除
+if [ "$(docker ps -a -q -f name=^/${CONTAINER_NAME}$)" ]; then
+  echo "Removing existing container: $CONTAINER_NAME"
+  docker rm -f "$CONTAINER_NAME"
+fi
+
+docker run --rm -i \
     --env DISPLAY=localhost:11.0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/root/.Xauthority:ro \
@@ -13,5 +19,5 @@ docker run --rm -it \
     --privileged \
     --net=host \
     $DOCKER_IMAGE \
-    bash
+    bash -c "source /opt/ros/humble/setup.bash && source /root/icart_ws/install/setup.bash && ros2 launch tang_bringup tang_bringup.launch.py"
 
